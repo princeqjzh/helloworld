4dcb06255bce75593cb03bb365ce6749d3e84ba0
qianjizhi <11547299@qq.com>
2018-04-14 20:13:34 +0800

    update Hello.java

--- src/main/java/com/mooc/Hello.java
+++ src/main/java/com/mooc/Hello.java
@@ -5,6 +5,6 @@ package com.mooc;
  */
 public class Hello {
     public static void main(String [] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World , ha ha ha !");
     }
 }
