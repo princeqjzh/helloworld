9685f663df16be4af1ac8c05587241ec6e659674
qianjizhi <11547299@qq.com>
2018-04-14 19:57:06 +0800

    init

---
+++ src/main/java/com/mooc/Hello.java
@@ -0,0 +1,10 @@
+package com.mooc;
+
+/**
+ * Created by jizhi.qian on 18/4/14.
+ */
+public class Hello {
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+    }
+}
